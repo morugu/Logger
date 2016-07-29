776b735fbdcccc84d09894d2fda976ad5b387e45
shoya <12c1055@gmail.com>
2016-07-29 19:46:54 +0900

    Initial Commit

---
+++ Example/Pods/Target Support Files/Logger/Logger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Logger : NSObject
+@end
+@implementation PodsDummy_Logger
+@end
---
+++ Example/Pods/Target Support Files/Logger/Logger-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double LoggerVersionNumber;
+FOUNDATION_EXPORT const unsigned char LoggerVersionString[];
+
